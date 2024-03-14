8632476e18004767be0b87f04cbad220bea5d3b8
sskiles66 <stephentskiles@hotmail.com>
2024-03-14 14:18:24 -0600

    Made user routes skeleton and updated swagger

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
 
+router.get("/:id", (req, res) => {
+    res.send.json("For getting one user")
+});
+
+router.post("/", (req, res) => {
+    res.send.json("For posting one user")
+});
+
+router.put("/:id", (req, res) => {
+    res.send.json("For updating one user")
+});
+
+router.delete("/:id", (req, res) => {
+    res.send.json("For deleting one user")
+});
+
 module.exports = router;
