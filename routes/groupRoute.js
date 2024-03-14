93ae1f9434b5003da9a13fd65eedfa805c6c4adf
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-03-14 14:11:39 -0600

    setting and group routes

--- routes/groupRoute.js
+++ routes/groupRoute.js
@@ -1,3 +1,28 @@
 const router = require("express").Router();
+// //POST /group
+ router.post("/", (req, res) => {
+   res.send("POST group");
+ });
+
+// PUT /group/:groupId
+router.put("/:groupId", (req, res) => {
+  res.send("PUT group");
+});
+
+// GET /group/
+router.get("/", (req, res) => {
+  res.send("GET group");
+});
+
+// GET /group/:groupId
+router.get("/:groupId", (req, res) => {
+  res.send("GET group");
+});
+
+// DELETE /group/:groupId
+router.delete("/:groupId", (req, res) => {
+  res.send("DELETE group");
+});
+
 
 module.exports = router;
--- routes/settingsRoute.js
+++ routes/settingsRoute.js
@@ -1,3 +1,20 @@
 const router = require("express").Router();
+//GET/settings
+router.get("/", (req, res) => {
+  res.send("GET settings");
+});
+//POST/settings
+router.post("/", (req, res) => {
+  res.send("POST settings");
+});
+//PUT/settings/:settingId
+router.put("/:settingId", (req, res) => {
+  res.send("PUT settings");
+});
+
+//DELETE/settings/:settingId
+router.delete("/:settingId", (req, res) => {
+  res.send("DELETE settings");
+});
 
 module.exports = router;
