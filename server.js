1e04998acbc01422afc95961ea9d35b319607f29
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-03-14 13:26:52 -0600

    swagger setup

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const routes = require("./routes");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app.use(express.json());
  
 app.use("/", routes);
 
+// Swagger
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI).then(() => {
   // Listen for requests using Port number from .env file
   app.listen(process.env.PORT || 3000, () => {
